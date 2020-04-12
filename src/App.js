9a8c4752ac2484e11d343cbc48ca2bbfd1f87d1b
Niv B <nivb52@gmail.com>
2020-04-12 14:22:58 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './App.css';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 import LaunchesPast from './views/LaunchesPast';
+import AddUser from './views/AddUser';
+
 const client = new ApolloClient({
   uri: 'https://api.spacex.land/graphql/',
 });
@@ -11,12 +14,31 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="App">
-        <header className="App-header">on air</header>
-      </div>
-      <div className="App-container">
-        <LaunchesPast />
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            SpaceX Graph-QL
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/add-user">Add User</Link>
+                </li>
+              </ul>
+            </nav>
+
+          </header>
+        </div>
+        <div className="App-container">
+          <Switch>
+            <Route exact path="/" component={LaunchesPast} />
+            {/* <Route path="/launch/:id" component={LaunchDetails} /> */}
+            <Route path="/add-user" component={AddUser} />
+          </Switch>
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
