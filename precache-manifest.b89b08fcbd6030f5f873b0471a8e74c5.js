ab470a75f1387efe2775703dfc0b65b9942bf32b
Niv B <nivb52@gmail.com>
2020-04-13 15:34:23 +0300

    Updates

---
+++ precache-manifest.b89b08fcbd6030f5f873b0471a8e74c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a7afc1c91e66f144e05d2216305929a",
+    "url": "/React-GraphQL-Apollo-Client-/index.html"
+  },
+  {
+    "revision": "abf3ce66880313527aad",
+    "url": "/React-GraphQL-Apollo-Client-/static/css/main.5dab37bb.chunk.css"
+  },
+  {
+    "revision": "ed661db9ff2db7d3e902",
+    "url": "/React-GraphQL-Apollo-Client-/static/js/2.e06aa43c.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/React-GraphQL-Apollo-Client-/static/js/2.e06aa43c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf3ce66880313527aad",
+    "url": "/React-GraphQL-Apollo-Client-/static/js/main.07ccc944.chunk.js"
+  },
+  {
+    "revision": "2e88b39f75eae128c4b6",
+    "url": "/React-GraphQL-Apollo-Client-/static/js/runtime-main.4d9b5583.js"
+  },
+  {
+    "revision": "8ecb06c289a94bab09d3976a80daa5d6",
+    "url": "/React-GraphQL-Apollo-Client-/static/media/rocket-128px.8ecb06c2.svg"
+  }
+]);
